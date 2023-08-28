30ae702b2c9a2e397d10c1a77ceb0a9cf6ffb5b6
tcotidiane33 <tcotidiane33@gmail.com>
2023-08-28 11:08:36 +0000

    update file and delete index.html and folder public_html

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <html>
     <head>
-        <title>CinetPay-SDK-PHP</title>
+        <title>KONDROPAY</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
         <link rel="stylesheet" href="src/interface.css">
